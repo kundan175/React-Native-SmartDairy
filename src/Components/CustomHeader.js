554c0d6baf6dda820bd1e2daa549b84d680bdc81
kundan175 <kundansingh957632@gmail.com>
2023-04-22 15:58:41 +0530

    ios

--- src/Components/CustomHeader.js
+++ src/Components/CustomHeader.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import {View,Text, StyleSheet} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const CustomHeader = ({title,headerColor,textColor,textStyle,headerStyle}) => {
-return(
-    <View style={{
+const CustomHeader = ({
+  title,
+  headerColor,
+  textColor,
+  textStyle,
+  headerStyle,
+}) => {
+  return (
+    <View
+      style={{
         ...styles.headerContainer,
-        ...headerStyle, 
-        backgroundColor: headerColor || '#002047',}}>
-
-    <Text style={{
-        ...styles.textStyle,
-        ...textStyle,
-        color: textColor || '#FFFFFF',}}>{title}</Text>
+        ...headerStyle,
+        backgroundColor: headerColor || "#002047",
+      }}
+    >
+      <Text
+        style={{
+          ...styles.textStyle,
+          ...textStyle,
+          color: textColor || "#FFFFFF",
+        }}
+      >
+        {title}
+      </Text>
     </View>
-)
+  );
 };
 export default CustomHeader;
 
 const styles = StyleSheet.create({
-    headerContainer:{
-        padding:30,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-textStyle:{
-    fontSize:20,
-    fontWeight:'700'
-}
-})
+  headerContainer: {
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textStyle: {
+    fontSize: 22,
+    fontWeight: "700",
+  },
+});
