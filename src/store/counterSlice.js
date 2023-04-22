30eacbc013dc2cb7357bf91bdb8c33cae5ea0100
kundan175 <kundansingh957632@gmail.com>
2023-04-22 16:33:58 +0530

    kuch

---
+++ src/store/counterSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {},
+});
+
+export const {} = counterSlice.actions;
+export default counterSlice.reducer;
